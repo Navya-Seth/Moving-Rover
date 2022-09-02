2b70978076831015da3019301f541586fda258d0
Navya-Seth <91605093+Navya-Seth@users.noreply.github.com>
2022-09-02 20:16:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,9 +31,67 @@ function upload_rower(){
 function mykeydown(e){
     keypress = e.keyCode;
     console.log(keypress);
+    if (keypress == 38){
+        up();
+        console.log("up");
+    }
+
+    if (keypress == 40){
+        down();
+        console.log("down");
+    }
+
+    if (keypress == 37){
+        left();
+        console.log("left");
+    }
+
+    if (keypress == 39){
+        right();
+        console.log("right");
+    }
+
+
 }
 
 window.addEventListener("keydown",mykeydown);
+
+function up(){
+    if (rover_y >= 0){
+        rover_y = rover_y-10;
+        console.log("When Up Arrow Is Pressed , X = "+ rover_x + "Y = " + rover_y);
+        upload_backround();
+        upload_rower();
+    }
+}
+
+function down(){
+    if (rover_y <=500){
+        rover_y = rover_y+10;
+        console.log("When Down Arrow Is Pressed , X = "+ rover_x + "Y = " + rover_y);
+        upload_backround();
+        upload_rower();
+    }
+}
+
+function left(){
+    if (rover_x >= 0){
+        rover_x = rover_x-10;
+        console.log("When Left Arrow Is Pressed , X = "+ rover_x + "Y = " + rover_y);
+        upload_backround();
+        upload_rower();
+    }
+}
+
+function right(){
+    if (rover_x <= 700){
+        rover_x = rover_x+10;
+        console.log("When Right Arrow Is Pressed , X = "+ rover_x + "Y = " + rover_y);
+        upload_backround();
+        upload_rower();
+    }
+}
+
 
 
 
